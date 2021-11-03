130e7ab60f4d6303e7a0c2f0e16aa77a48b1f226
s11028834 <s11028834@students.duvalschools.org>
2021-11-03 08:47:56 -0400

    v0.1

---
+++ Student.java
@@ -0,0 +1,34 @@
+// Unit 2, Topic 1, Classes and Objects, Germari Cull, 11/3/21, v0.1
+
+public class Student {
+    // THESE ARE INSTANCE VARIABLES
+    private int studentID;
+    private int gradeLevel;
+    private double gradePointAverage;
+    private String studentName;
+}
+
+    // Creating a Constructor
+    public Student() {
+        this.studentID = 0;
+        this.gradeLevel = 0;
+        this.gradePointAverage = 0.0;
+        this.studentName = "";
+        // Generally this type of constructor makes a default copy.
+    }
+
+    // Value Constructor
+    public Student(int studentID, int gradeLevel, double GPA, String studentName) {
+        this.studentID = studentID;
+        this.gradeLevel = gradeLevel;
+        this.gradePointAverage = GPA;
+        this.studentName = studentName;
+
+    }
+
+    public Student()
+
+    public static void main(String[] args){
+        Student student0 = new Student();
+        Student student1 = new Student(11025564/ 12, 0.34, "Kali Stevens");
+    }
