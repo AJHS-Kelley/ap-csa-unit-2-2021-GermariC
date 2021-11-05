f9e7e742574c178d18ec0963febc51672f05e60f
s11028834 <s11028834@students.duvalschools.org>
2021-11-05 09:12:49 -0400

    V0.4

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-// Unit 2, Topic 1, Classes and Objects, Germari Cull, 11/3/21, v0.2
+// Unit 2, Topic 1, Classes and Objects, Germari Cull, 11/5/21, v0.4
 
 public class Student {
     // THESE ARE INSTANCE VARIABLES
@@ -8,7 +8,6 @@ public class Student {
     private int gradeLevel;
     private double gradePointAverage;
     private String studentName;
-}
 
     // Creating a Constructor
     public Student() {
@@ -33,10 +32,69 @@ public class Student {
         this.studentName = studentName;
         this.gradeLevel = 10;
         this.gradePointAverage = 3.8;
+        // More than one constructor is known as OVERLOADING
 
     }
 
-    public static void main(String[] args){
-        Student student0 = new Student();
-        Student student1 = new Student(11025564, 12, 0.34, "Kali Stevens");
+    // Creating Methods
+
+    public static void calcGPA() {
+        int grade0;
+        int grade1;
+        int grade2;
+        int grade3;
+        double newGPA;
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What is the first grade?");
+        grade0 = myScanner.nextInt();
+        System.out.println("What is the second grade?");
+        grade1 = myScanner.nextInt();
+        System.out.println("What is the third grade?");
+        grade2 = myScanner.nextInt();
+        System.out.println("What is the fourth grade?");
+        grade3 = myScanner.nextInt();
+
+        newGPA = (grade0 + grade1 + grade2 + grade3) / 4;
+        System.out.print("The new GPA is");
+        System.out.println(newGPA);
     }
+
+    // Parameters and Arguments
+    public static void assignLunch(int gradeLevel){
+        String lunchPeriod;
+
+        if (gradeLevel == 9){
+            lunchPeriod = "First Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 10){
+            lunchPeriod = "Second Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 11){
+            lunchPeriod = "Third Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else (gradeLevel == 12){
+            lunchPeriod = "Off-Campus Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        /*
+         * Student student0 = new Student(); Student student1 = new Student(11025564,
+         * 12, 0.34, "Kali Stevens");
+         * 
+         * System.out.print
+         */
+
+        assignLunch(9);
+    }
+}
